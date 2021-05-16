18240eada2bd7605289fb29d37ba24e09abd3e6f
Phiju <PhijuFile@phijus-air.lan>
2021-05-16 10:34:36 +0800

    UI theme to black

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,108 +1,44 @@
 import React from "react";
-import skin1 from "./skin1.jpeg";
 import "./Home.css";
 import Footer from "../components/Footer";
-import Slider from "react-slick";
-import onne1 from "./onne1.jpeg";
-import onne2 from "./onne2.jpeg";
-import onne3 from "./onne3.jpeg";
-import onne4 from "./onne4.jpeg";
+import black3 from "./black3.png";
+import black2 from "./black2.jpeg";
 
 const Home = () => {
-  function SampleNextArrow(props) {
-    const { className, style, onClick, to } = props;
-    return (
-      <button
-        type="button"
-        onClick={onClick}
-        className={`button button--text button--icon ${className}`}
-      ></button>
-    );
-  }
-  function SamplePrevArrow(props) {
-    const { className, style, onClick, to } = props;
-    return (
-      <button
-        type="button"
-        onClick={onClick}
-        className={`button button--text button--icon ${className}`}
-      ></button>
-    );
-  }
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    cssEase: "linear",
-    nextArrow: <SampleNextArrow to="next" />,
-    prevArrow: <SamplePrevArrow to="prev" />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 300,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
     <div>
-      <img className="section-one" alt="fruit" src={skin1}></img>
+      <img src={black3} alt="" className="section-one"></img>
       <div className="section-two">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            fontSize: "1.5rem",
-          }}
-        >
+        <div className="header-container">
           <h3>New Arrivals</h3>
         </div>
-
-        <Slider {...settings}>
-          <div>
-            <img className="img" alt="green2" src={onne1}></img>
+        <div className="outer-sub-container">
+          <div className="inner-sub-container">
+            <img className="img" alt="black2" src={black2}></img>
+            <div className="text-container">
+              <h3>Rock On Music Festival</h3>
+              <p style={{ color: "white" }}>Singapore</p>
+            </div>
           </div>
-          <div>
-            <img className="img" alt="green2" src={onne2}></img>
+          <div className="inner-sub-container">
+            <img className="img" alt="black2" src={black2}></img>
+            <div className="text-container">
+              <h3>Rock On Music Festival</h3>
+              <p style={{ color: "white" }}>Dubai</p>
+            </div>
           </div>
-          <div>
-            <img className="img" alt="green2" src={onne3}></img>
+          <div className="inner-sub-container">
+            <img className="img" alt="black2" src={black2}></img>
+            <div className="text-container">
+              <h3>Rock On Music Festival</h3>
+              <p style={{ color: "white" }}>Japan</p>
+            </div>
           </div>
-          <div>
-            <img className="img" alt="green2" src={onne4}></img>
-          </div>
-          <div>
-            <img className="img" alt="green2" src={skin1}></img>
-          </div>
-        </Slider>
+        </div>
       </div>
       <div className="section-three">
         <div className="left">
-          <img className="last-img" alt="green2" src={skin1}></img>
+          <img className="last-img" alt="green2" src={black2}></img>
         </div>
         <div className="right">
           <p className="text">
